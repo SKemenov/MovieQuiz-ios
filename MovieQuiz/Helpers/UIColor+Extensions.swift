034861beb416e45067a0c76195c74afa47aef453
Sergey Kemenov <skemenov@icloud.com>
2023-04-28 23:28:49 +0300

    added comments

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,11 +1,12 @@
 import UIKit
 
 extension UIColor {
+//    use these syntax to add more custom color's names into UIColor, likes UIColor.ypBlack
     static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
     static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
     static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
     static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
     static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
-// opacity 60% of the the original color of the background
+// opacity 60% of the the original color of the background or darkGray as alternative
     static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
 }
