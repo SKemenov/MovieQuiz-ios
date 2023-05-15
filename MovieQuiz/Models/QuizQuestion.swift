6f14a4f401cd6adcf5c97be33860e17f0530cf20
Sergey Kemenov <126358486+SKemenov@users.noreply.github.com>
2023-05-15 13:06:45 +0300

    Added horizontal rules for DocC in QuizQuestion.swift

--- MovieQuiz/Models/QuizQuestion.swift
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -15,7 +15,13 @@ import Foundation
 /// The `QuizQuestion` structure has the following properties:
 ///
 /// - term **name: String**: Used to represent a movie name (and using as a picture name too).
+///
+/// *****
+///
 /// - term **text: String**: Used to represent a question about the current movie.
+///
+/// *****
+///
 /// - term **correctAnswer: Bool**: Used to represent the correct asnwer for the question is the `true` or `false`.
 ///
 /// - Important: The `QuizQuestion` structure has no methods.
