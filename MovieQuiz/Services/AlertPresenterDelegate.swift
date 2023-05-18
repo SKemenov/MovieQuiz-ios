e2b399fe2ccef46d68cbf1c4e2b39902c2d2ce9b
Sergey Kemenov <126358486+SKemenov@users.noreply.github.com>
2023-05-18 15:59:33 +0300

    Update AlertPresenterDelegate.swift

--- MovieQuiz/Services/AlertPresenterDelegate.swift
+++ MovieQuiz/Services/AlertPresenterDelegate.swift
@@ -10,12 +10,12 @@ import Foundation
 //  MARK: - Protocols
 //
 //
-///// This's a delegate to control receiving questions from our Factory.
-/////
-///// To be compatible should implement `didReceiveNextQuestion(question:)` method.
-//protocol QuestionFactoryDelegate: AnyObject {
-//    /// Request a callback method for updating UI after loading the question.
-//    /// - Parameter question: a question or `nil`
-//    func didReceiveNextQuestion(question: QuizQuestion?)
-//}
+/// This's a delegate to control receiving alert's data from `AlertPresenter`.
+///
+/// To be compatible should implement `didReceiveAlertFor(alert:)` method.
+protocol AlertPresenterDelegate: AnyObject {
+    /// Request a delegate method for updating UI after loading the question.
+    /// - Parameter alert: an alert or `nil`
+    func didReceiveAlertFor(alert: AlertModel?)
+}
 
