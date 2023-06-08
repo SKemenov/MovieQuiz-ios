c8ff642ab0097e42918df83505ba546ce82c5a2c
Sergey Kemenov <skemenov@icloud.com>
2023-06-08 09:29:18 +0300

    finished `MoviesLoader`

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,46 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Sergey Kemenov on 08.06.2023.
+//
+
+import Foundation
+
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+/// A service to connecting to IMDb API and loading Top250Movies data into `MostPopularMovies`
+struct MovieLoader: MoviesLoading {
+    private let networkClient = NetworkClient()
+    private let imdbUrl = "https://imdb-api.com/en/API/Top250Movies/"
+    private let imdbToken = "k_gbe4ep0b"
+    
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: imdbUrl + imdbToken) else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+                case .success(let data):
+                    do {
+                        let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                        handler(.success(mostPopularMovies))
+                    }
+                    catch {
+                        handler(.failure(error))
+                    }
+                case .failure(let error):
+                    handler(.failure(error))
+            }
+        }
+    }
+    
+    
+}
